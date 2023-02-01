3efbb01e5e55fe260e1c5eabd1da5e6a06318cfd
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-02-01 22:12:27 +0200

    Testing done

---
+++ 10_week10_Testing/Test-todo/src/App.test.js
@@ -0,0 +1,39 @@
+import { render, screen, fireEvent } from'@testing-library/react'
+import'@testing-library/jest-dom/extend-expect';
+import TodoTable from'./TodoTable';
+import App from'./App';
+
+
+test('renders todotable', () => {
+  const row = [{desc: 'Go to coffee', date: '24.01.2021'}];  
+  render(< TodoTable todos= {row} />);
+  const tablecell = screen.getByText(/go to coffee/i);  
+  expect(tablecell).toBeInTheDocument();
+});
+
+test('add todo',() => {  
+  render(<App />);
+  const desc = screen.getByPlaceholderText('Description'); 
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');  
+  fireEvent.change(date, { target: { value: '29.01.2021' } });
+  const button = screen.getByText('Add'); 
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/Go to coffee/i);  
+  expect(tablecell).toBeInTheDocument();
+});
+
+test('add and clear',() => {
+  render(<App />);
+ const desc = screen.getByPlaceholderText('Description'); 
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');  
+  fireEvent.change(date, { target: { value: '29.01.2021' } });
+  const button = screen.getByText('Add'); 
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/Go to coffee/i);  
+  expect(tablecell).toBeInTheDocument();
+  const clear = screen.getByText('Clear');
+  fireEvent.click(clear);
+  expect(tablecell).not.toBeInTheDocument();
+})
