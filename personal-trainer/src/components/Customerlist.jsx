a8af45218f67fabda8890cf22f6f15ca1faa1073
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-02-11 19:03:25 +0200

    more functions added

--- personal-trainer/src/components/Customerlist.jsx
+++ personal-trainer/src/components/Customerlist.jsx
@@ -3,7 +3,8 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
 import Button from '@mui/material/Button';
-import dayjs from 'dayjs';
+import Addcustomer from './Addcustomer';
+import Addtraining from './AddTraining';
 
 
 export function CustomerList() {
@@ -21,6 +22,34 @@ export function CustomerList() {
   };
 
 
+  function handleCellValueChanged(params) {
+    const data = { ...params.data };
+    const updatedData = { ...data, [params.colDef.field]: params.newValue };
+  
+    updateCustomer(updatedData)
+      .then(() => {
+        console.log("Customer data updated successfully!");
+      })
+      .catch((error) => {
+        console.error("Error updating customer data:", error);
+      });
+  }
+  
+  async function updateCustomer(customerData) {
+    const response = await fetch(`${customerData.links[0].href}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(customerData),
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Failed to update customer data: ${response.statusText}`);
+    }
+  }
+
+
 
   const deleteCustomer = (link) => {
     if (window.confirm('Are you sure?')) {
@@ -33,30 +62,59 @@ export function CustomerList() {
     }
   };
 
+  const saveCustomer = (customer) => {
+		fetch('http://traineeapp.azurewebsites.net/api/customers', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(customer),
+		})
+			.then((response) => fetchData())
+			.catch((err) => console.error(err));
+	};
 
+
+  const saveTraining = (training) => {
+    fetch('http://traineeapp.azurewebsites.net/api/trainings', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(training),
+    })
+    .then((response) => fetchData())
+    .catch((err) => console.error(err));
+  };
+  
 
   const columns = [
-    { headerName: "Last Name", field: "lastname", sortable: true, filter: true  },
-    { headerName: "First Name", field: "firstname", sortable: true, filter: true  },
-    { headerName: "Email", field: "email", sortable: true, filter: true  },
-    { headerName: "Phone", field: "phone", sortable: true, filter: true  },
-    { headerName: "Address", field: "streetaddress", sortable: true, filter: true  },
-    { headerName: "Postcode", field: "postcode", sortable: true, filter: true  },
-    { headerName: "City", field: "city", sortable: true, filter: true  },
+    { headerName: "Last Name", field: "lastname", sortable: true, filter: true, editable: true, onCellValueChanged: handleCellValueChanged },
+    { headerName: "First Name", field: "firstname", sortable: true, filter: true, editable: true, onCellValueChanged: handleCellValueChanged },
+    { headerName: "Email", field: "email", sortable: true, filter: true, editable: true, onCellValueChanged: handleCellValueChanged },
+    { headerName: "Phone", field: "phone", sortable: true, filter: true, editable: true, onCellValueChanged: handleCellValueChanged },
+    { headerName: "Address", field: "streetaddress", sortable: true, filter: true, editable: true, onCellValueChanged: handleCellValueChanged },
+    { headerName: "Postcode", field: "postcode", sortable: true, filter: true, editable: true, onCellValueChanged: handleCellValueChanged },
+    { headerName: "City", field: "city", sortable: true, filter: true, editable: true, onCellValueChanged: handleCellValueChanged },
     { headerName: "Action",
       cellRendererFramework: (props) => (
-        <Button
-          onClick={() => deleteCustomer(props.data.links[0].href)}
-        >
+        
+        <Button onClick={() => deleteCustomer(props.data.links[0].href)}>
           Delete
         </Button>
+        
+        
       ),
-    },
+    }, { headerName: "Add",
+    cellRendererFramework: (props) => (
+      <>
+      <Addtraining saveTraining={saveTraining} />
+      </>
+    ),
+  },
   ];
+
   
 
   return (
     <div className="ag-theme-material" style={{ height: '800px', width: '1800px', margin:'auto' }}>
+      <Addcustomer saveCustomer={saveCustomer}/>
       <AgGridReact columnDefs={columns} rowData={customers} enableSorting={true}
       enableFilter={true} />
     </div>
--- personal-trainer/src/components/Training.jsx
+++ personal-trainer/src/components/Training.jsx
@@ -12,7 +12,10 @@ import dayjs from 'dayjs';
 export function Traininglist() {
     const [trainings, setTrainings] = useState([]);
 
-    useEffect(() => {
+    useEffect(() => fetchData(), []);
+
+
+    const fetchData = () => {
     fetch("http://traineeapp.azurewebsites.net/api/trainings")
         .then(res => res.json())
         .then(data => {
@@ -25,16 +28,27 @@ export function Traininglist() {
                 duration: training.duration,
                 date: dayjs(training.date).format("DD-MM-YYYY HH.mm a"),
                 time: dayjs(training.date).format("HH.mm"),
-                customerName: `${customerData.firstname} ${customerData.lastname}`
-
+                customerName: `${customerData.firstname} ${customerData.lastname}`,
+                link: training.links[0].href
                 }))
             )
         )
             .then(formattedTrainings => setTrainings(formattedTrainings))
             .catch(error => console.error(error));
         });
-    }, []);
+    }
 
+
+    const deleteTraining = (link) => {
+        if (window.confirm('Are you sure?')) {
+          fetch(link, { method: 'DELETE' })
+            .then((response) => fetchData())
+            .catch((err) => console.error(err));
+          alert('Training is deleted!');
+        } else {
+          alert('Nothing deleted.');
+        }
+      };
    
     const columns = [
        
@@ -42,7 +56,11 @@ export function Traininglist() {
         { headerName: "Date", field: "date", sortable: true, filter: true  },
         { headerName: "Duration (minutes)", field: "duration", sortable: true, filter: true  },
         { headerName: "Customer Name", field: "customerName", sortable: true, filter: true  },
-
+        { headerName: "action" ,field: "link",
+        cellRenderer: ({ value }) => (
+            <Button size="large" onClick={() => deleteTraining(value)}>Delete</Button>
+            ), },
+      
       ];
       
   
